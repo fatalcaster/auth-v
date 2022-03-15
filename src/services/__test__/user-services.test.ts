8be3430da20c0ac8bd8cdcf39cf8200d1037eb38
fatalcaster <milovicvojin@yahoo.com>
2022-03-15 21:11:10 +0100

    added and tested user-services

---
+++ src/services/__test__/user-services.test.ts
@@ -0,0 +1,122 @@
+import { ID } from "../../helpers/ID";
+import { Service } from "../../models/entity/service";
+import {
+  createService,
+  deleteService,
+  getServiceById,
+} from "../service-services";
+import {
+  createUser,
+  deleteUser,
+  getManyUsers,
+  getUserById,
+  updateUserEmail,
+  updateUserPassword,
+} from "../user-services";
+
+const randomString = () => {
+  return Math.random().toString(36).substring(2, 7);
+};
+
+const createServiceProvider = async (user?: string) => {
+  const email = `${user || "service"}@service.com`;
+  const service = await createService({ email: email });
+  expect(ID.isValid(service.id)).toEqual(true);
+  return service;
+};
+const createValidUser = async (username?: string, service?: Service) => {
+  if (!service) service = await createServiceProvider();
+  const email = `${username || "user"}@user.com`;
+  const user = await createUser({ email: email, service: service });
+  expect(ID.isValid(user.id)).toEqual(true);
+  return { user, service };
+};
+
+it("creates a valid user", async () => {
+  const service = await createServiceProvider();
+  const user = await createUser({ email: "user@test.com", service: service });
+  expect(ID.isValid(user.id)).toEqual(true);
+});
+
+it("creates a valid user", async () => {
+  const service = await createServiceProvider();
+  const user = await createUser({ email: "user@test.com", service: service });
+  expect(ID.isValid(user.id)).toEqual(true);
+});
+
+it("creates multiple valid users at once", async () => {
+  const service = await createServiceProvider();
+  const arr = [
+    createValidUser(randomString(), service),
+    createValidUser(randomString(), service),
+    createValidUser(randomString(), service),
+    createValidUser(randomString(), service),
+    createValidUser(randomString(), service),
+  ];
+  await Promise.all(arr);
+  for (let i = 0; i < arr.length; i++) {
+    expect(ID.isValid((await arr[i]).user.id)).toEqual(true);
+  }
+});
+
+it("attempts to get a user that doesn't exist", async () => {
+  const service = await getUserById(ID.generate());
+  expect(service).toEqual(null);
+});
+
+it("attempts to get users from a non-existent service", async () => {
+  await createValidUser();
+  const users = await getManyUsers({ serviceId: ID.generate() });
+  expect(users).toEqual([]);
+});
+
+it("gets users from an existing service", async () => {
+  const { service } = await createValidUser();
+  await createValidUser("random", service);
+
+  const users = await getManyUsers({ serviceId: service.id });
+  expect(users.length).toEqual(2);
+});
+
+it("gets users from an existing service", async () => {
+  const { user } = await createValidUser();
+
+  const get_user = await getUserById(user.id);
+  expect(get_user?.id).toEqual(user.id);
+});
+
+it("deletes all users of the deleted service", async () => {
+  const { service, user } = await createValidUser("something");
+  expect(ID.isValid(user.id)).toEqual(true);
+  await deleteService(service.id);
+  const deleted_user = await getUserById(user.id);
+  expect(deleted_user).toEqual(null);
+});
+
+it("deletes an existing user", async () => {
+  const { service, user } = await createValidUser("something");
+  expect(ID.isValid(user.id)).toEqual(true);
+  await deleteUser(user.id);
+  const deleted_user = await getUserById(user.id);
+  expect(deleted_user).toEqual(null);
+  const get_service = await getServiceById(service.id);
+  expect(ID.isValid(get_service?.id)).toEqual(true);
+});
+
+it("updates user password", async () => {
+  const { user } = await createValidUser("user");
+  const new_user = await updateUserPassword(user.id, "password");
+  expect(user.password).not.toEqual(new_user?.password);
+});
+
+it("attempts to update user email with an already existing email within the same service", async () => {
+  const service = await createServiceProvider();
+  const { user: user1 } = await createValidUser("user1", service);
+  const { user: user2 } = await createValidUser("user2", service);
+  try {
+    await updateUserEmail(user1.id, user2.email);
+    expect(true).toEqual(false);
+  } catch (err) {
+    expect(true).toEqual(true);
+  }
+});
---
+++ src/services/user-services.ts
@@ -0,0 +1,140 @@
+import { User } from "../models/entity/user";
+import { getConnection } from "typeorm";
+import { NotFoundError } from "../errors/not-found-error";
+import {
+  CustomBaseEntity,
+  RequiredEntityProps,
+} from "../models/entity/base-entity";
+import bcrypt from "bcrypt";
+const QUERY_LIMIT = 50;
+
+const getUserById = async (id: string) => {
+  const user = await getConnection()
+    .getRepository(User)
+    .createQueryBuilder("user")
+    .where("user.id = :id", { id: id })
+    .innerJoinAndSelect("user.service", "service")
+    .select([
+      "user.id",
+      "user.email",
+      "user.password",
+      "user.created_at",
+      "service.authMethod",
+    ])
+    .getOne();
+  return user || null;
+};
+
+class GetManyUsersFilter {
+  limit?: number = QUERY_LIMIT;
+  skip?: number = 0;
+  inIds?: string[] = undefined;
+  serviceId?: string;
+  order?: "ASC" | "DESC" = "DESC";
+  orderBy?: keyof User | keyof CustomBaseEntity = "created_at";
+  constructor(filter?: GetManyUsersFilter) {
+    this.limit = filter?.limit || QUERY_LIMIT;
+    this.skip = filter?.skip || 0;
+    this.inIds = filter?.inIds || undefined;
+    this.serviceId = filter?.serviceId || undefined;
+    this.order = filter?.order || "DESC";
+    this.orderBy = filter?.orderBy || "created_at";
+  }
+}
+const getManyUsers = async (filters?: GetManyUsersFilter) => {
+  filters = new GetManyUsersFilter(filters);
+  // const query = getConnection().getRepository(User).createQueryBuilder("user");
+  const users = await getConnection().manager.find(User, {
+    loadRelationIds: true,
+    select: ["id", "email", "created_at", "service"],
+    ...(filters.serviceId && {
+      where: {
+        service: filters.serviceId,
+      },
+    }),
+    skip: filters.skip,
+    take: filters.limit,
+    order: {
+      [filters.orderBy!]: filters.order,
+    },
+  });
+  return users;
+};
+
+const SALT_ROUNDS = 6;
+
+const createUser = async (new_user: RequiredEntityProps<User>) => {
+  const user = new User();
+  user.email = new_user.email;
+  user.password = new_user.password
+    ? await bcrypt.hash(new_user.password!, SALT_ROUNDS)
+    : undefined;
+  user.service = new_user.service;
+  await getConnection().manager.save(User, user);
+  return user;
+};
+
+const updateUser = async (
+  user: User | string | null,
+  update: Partial<RequiredEntityProps<User>>
+) => {
+  if (typeof user === "string") {
+    user = await getUserById(user);
+    if (!user) {
+      return null;
+    }
+  }
+  let key: keyof User;
+  for (key in user) {
+    if (key in update && key !== "id") {
+      // @ts-ignore
+      user[key] = update[key] || user[key];
+    }
+  }
+  await getConnection().manager.save(User, user!);
+  return user;
+};
+
+const deleteUser = async (user: User | string | null) => {
+  if (typeof user === "string") {
+    user = await getUserById(user);
+    if (!user) {
+      throw new NotFoundError();
+    }
+  }
+  await getConnection().manager.remove(User, user!);
+};
+
+const updateUserPassword = async (id: string, new_password: string) => {
+  const user = await getConnection()
+    .manager.save(User, {
+      id: id,
+      password: await bcrypt.hash(new_password, SALT_ROUNDS),
+    })
+    .catch(() => {
+      return null;
+    });
+  return user || null;
+};
+
+const updateUserEmail = async (id: string, email: string) => {
+  const user = await getConnection()
+    .manager.save(User, {
+      id: id,
+      email: email,
+    })
+    .catch(() => {
+      return null;
+    });
+  return user || null;
+};
+
+export {
+  getUserById,
+  getManyUsers,
+  createUser,
+  deleteUser,
+  updateUser,
+  updateUserPassword,
+  updateUserEmail,
+};
